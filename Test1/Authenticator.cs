701dde702c5aa7930a08ff841430ad1fedc13c7d
CORP\Geethu.Adhikarveedu <geethu.adhikarveedu@neudesic.com>
2017-05-29 21:10:50 +0530

    minor changes

--- Test1/Authenticator.cs
+++ Test1/Authenticator.cs
@@ -13,8 +13,8 @@ namespace Test1
         {
             #region variables
 
-            string accesskeyid = "AKIAJHYYM27C3VEXKFNQ";
-            string secretaccesskeyid = "ywh9ceZAbu5APUWtatH7LLq44MZ2c932dVUGfLlY";
+            string accesskeyid = "";
+            string secretaccesskeyid = "";
             var region = Amazon.RegionEndpoint.USEast1;
             var lex_client = new AmazonLexClient(accesskeyid, secretaccesskeyid, region);
             var byteArray = args;
